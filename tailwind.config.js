cfa45a6163c7a6efb67a86713129a720753961ab
Khalos Moscato <khalos.moscato@icloud.com>
2024-11-13 11:45:53 +0000

    feat: create colour scheme for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'electric-yellow': '#FFCB05',
+        'midnight-blue': '#2A4D69',
+        'soft-white': '#F8F9FA',
+        'grass-green': '#3DA35D',
+        'slate-gray': '#5D737E',
+      },
+    },
   },
   plugins: [],
 }
