55d51687d020facd0420674de64062e9b71feac3
maxitect <maxime.downe@gmail.com>
2024-11-18 17:10:37 +0000

    feat: add like feature for Pokemon.tsx, users can like and see liked pokemon from the database

--- src/pages/Pokémon.tsx
+++ src/pages/Pokémon.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "../utils/fetchData";
 import { Pokemon } from "../utils/types";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 export default function Pokémon() {
   const [pokemon, setPokemon] = useState<Pokemon[]>([]);
@@ -13,20 +14,54 @@ export default function Pokémon() {
     fetchPokemon();
   }, []);
 
+  const handleLike = async (poke: Pokemon) => {
+    const likesCount =
+      typeof poke.likes === "number"
+        ? poke.likes
+        : parseInt(poke.likes || "0", 10);
+    const updatedPoke = { ...poke, likes: likesCount + 1 };
+
+    try {
+      await fetchData(
+        `pokemon/${poke.id}`,
+        "PATCH",
+        JSON.stringify({ likes: updatedPoke.likes })
+      );
+      setPokemon((prev) =>
+        prev.map((p) => (p.id === poke.id ? updatedPoke : p))
+      );
+    } catch (error) {
+      console.error("Error updating likes:", error);
+    }
+  };
+
   return (
     <main className="bg-soft-white text-midnight-blue p-8">
-      <h2 className="text-3xl font-bold text-center">Pokémon Collection</h2>
+      <h2 className="text-3xl font-bold text-center">Pokémon</h2>
       <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {pokemon.map((poke) => (
           <div
             key={poke.id}
             className="bg-white p-4 rounded-lg shadow-md text-center"
           >
-            <img src={poke.imageUrl} alt={poke.name} className="w-24 mx-auto" />
+            <img src={poke.imageUrl} alt={poke.name} className="h-52 mx-auto" />
             <h3 className="text-xl font-semibold mt-4 capitalize">
               {poke.name}
             </h3>
             <p className="text-sm text-slate-gray">#{poke.id}</p>
+            <button
+              onClick={() => handleLike(poke)}
+              className="flex items-center"
+            >
+              {poke.likes == 0 ? (
+                <FaRegHeart className="text-black" />
+              ) : (
+                <FaHeart className="text-red-500" />
+              )}
+              <p className="ml-2 text-sm text-midnight-blue">
+                {poke.likes || 0} Likes
+              </p>
+            </button>
           </div>
         ))}
       </div>
