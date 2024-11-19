ab51d0f73ccddf373227c29337ef2e72e8ff1540
Khalos Moscato <khalos.moscato@icloud.com>
2024-11-19 10:55:25 +0000

    fix: pokemon page is now using dark mode

--- src/pages/Pokémon.tsx
+++ src/pages/Pokémon.tsx
@@ -36,29 +36,31 @@ export default function Pokémon() {
   };
 
   return (
-    <main className="bg-soft-white text-midnight-blue p-8">
+    <main className="bg-soft-white dark:bg-dark-charcoal text-midnight-blue dark:text-soft-white p-8">
       <h2 className="text-3xl font-bold text-center">Pokémon</h2>
       <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {pokemon.map((poke) => (
           <div
             key={poke.id}
-            className="bg-white p-4 rounded-lg shadow-md text-center"
+            className="bg-white dark:bg-midnight-blue p-4 rounded-lg shadow-md text-center"
           >
             <img src={poke.imageUrl} alt={poke.name} className="h-52 mx-auto" />
             <h3 className="text-xl font-semibold mt-4 capitalize">
               {poke.name}
             </h3>
-            <p className="text-sm text-slate-gray">#{poke.id}</p>
+            <p className="text-sm text-slate-gray dark:text-soft-white">
+              #{poke.id}
+            </p>
             <button
               onClick={() => handleLike(poke)}
               className="flex items-center"
             >
               {poke.likes == 0 ? (
-                <FaRegHeart className="text-black" />
+                <FaRegHeart className="text-black dark:text-soft-white" />
               ) : (
                 <FaHeart className="text-red-500" />
               )}
-              <p className="ml-2 text-sm text-midnight-blue">
+              <p className="ml-2 text-sm text-midnight-blue dark:text-soft-white">
                 {poke.likes || 0} Likes
               </p>
             </button>
